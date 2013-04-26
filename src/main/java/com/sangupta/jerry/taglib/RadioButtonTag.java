ae79c8e6b3809b3b40b88d0520d8cbb9a17be80c
Sandeep Gupta <sandy.pec@gmail.com>
2013-04-26 11:59:00 +0530

    Added HTML radio INPUT tag for creating faster JSPs

---
+++ src/main/java/com/sangupta/jerry/taglib/RadioButtonTag.java
@@ -0,0 +1,134 @@
+/**
+ *
+ * jerry - Common Java Functionality
+ * Copyright (c) 2012, Sandeep Gupta
+ * 
+ * http://www.sangupta/projects/jerry
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * 		http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+
+package com.sangupta.jerry.taglib;
+
+import java.io.IOException;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.tagext.SimpleTagSupport;
+
+import com.sangupta.jerry.util.AssertUtils;
+
+/**
+ * @author sangupta
+ *
+ */
+public class RadioButtonTag extends SimpleTagSupport {
+	
+	private String id;
+	
+	private String name;
+	
+	private String value;
+	
+	private String current;
+	
+	/**
+	 * @see javax.servlet.jsp.tagext.SimpleTagSupport#doTag()
+	 */
+	@Override
+	public void doTag() throws JspException, IOException {
+		JspWriter out = getJspContext().getOut();
+		
+		out.write("<input type=\"radio\" ");
+		
+		if(AssertUtils.isNotEmpty(this.id)) {
+			out.write("id=\"");
+			out.write(this.id);
+			out.write("\" ");
+		}
+		
+		out.write("name=\"");
+		out.write(this.name);
+		out.write("\" ");
+		
+		out.write("value=\"");
+		out.write(this.value);
+		out.write("\" ");
+		
+		if(this.value.equals(this.current)) {
+			out.write("checked=\"checked\" ");
+		}
+		
+		out.write(">\n");
+	}
+
+	// Usual accessors follow
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the value
+	 */
+	public String getValue() {
+		return value;
+	}
+
+	/**
+	 * @param value the value to set
+	 */
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public String getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the current
+	 */
+	public String getCurrent() {
+		return current;
+	}
+
+	/**
+	 * @param current the current to set
+	 */
+	public void setCurrent(String current) {
+		this.current = current;
+	}
+	
+}
