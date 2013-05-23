d8e5461a9dedd0b8efd657a0c8676b3e1e071470
Sandeep Gupta <sandy.pec@gmail.com>
2013-05-23 19:29:03 +0530

    Added HTTP status codes that can be used when working with network calls.

--- src/main/java/com/sangupta/jerry/http/HttpStatusCode.java
+++ src/main/java/com/sangupta/jerry/http/HttpStatusCode.java
@@ -22,15 +22,108 @@
 package com.sangupta.jerry.http;
 
 /**
+ * Constants regarding various HTTP codes. The basic values have been copied from
+ * the W3C page at http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html.
+ * 
+ * Additional codes have been copied from the Wikipedia page at,
+ * http://en.wikipedia.org/wiki/List_of_HTTP_status_codes
+ * 
+ * 
  * @author sangupta
  *
  */
 public class HttpStatusCode {
 	
+	// success codes
+	
+	public static final int CREATED = 201;
+	
+	public static final int ACCEPTED = 202;
+	
+	public static final int NON_AUTHORITATIVE_INFORMATION = 203;
+	
+	public static final int NO_CONTENT = 204;
+	
+	public static final int RESET_CONTENT = 205;
+	
+	public static final int PARTIAL_CONTENT = 206;
+	
+	// redirection codes
+	
+	public static final int MULTIPLE_CHOICES = 300;
+	
+	public static final int MOVED_PERMANENTLY = 301;
+	
+	public static final int FOUND = 302;
+	
+	public static final int SEE_OTHER = 303;
+	
+	public static final int NOT_MODIFIED = 304;
+	
+	public static final int USE_PROXY = 305;
+	
+	public static final int TEMPORARY_REDIRECT = 307;
+	
+	public static final int PERMANENT_REDIRECT = 308;
+	
+	// client error codes
+
+	public static final int BAD_REQUEST = 400;
+	
+	public static final int UNAUTHORIZED = 401;
+	
+	public static final int PAYMENT_REQUIRED = 402;
+	
+	public static final int FORBIDDEN = 403;
+	
 	public static final int NOT_FOUND = 404;
+	
+	public static final int METHOD_NOT_ALLOWED = 405;
+	
+	public static final int NOT_ACCEPTABLE = 406;
+	
+	public static final int PROXY_AUTHENTICATION_REQUIRED = 407;
+	
+	public static final int REQUEST_TIMEOUT = 408;
 	
 	public static final int CONFLICT = 409;
 	
 	public static final int GONE = 410;
+	
+	public static final int LENGTH_REQUIRED = 411;
+	
+	public static final int PRECONDITION_FAILED = 412;
+	
+	public static final int REQUEST_ENTITY_TOO_LARGE = 413;
+	
+	public static final int REQUEST_URI_TOO_LONG = 414;
+	
+	public static final int UNSUPPORTED_MEDIA_TYPE = 415;
+	
+	public static final int REQUEST_RANGE_NOT_SATISFIABLE = 416;
+	
+	public static final int EXPECTATION_FAILED = 417;
+	
+	public static final int UPGRADE_REQUIRED = 426;
+	
+	public static final int PRECONDITION_REQUIRED = 428;
+	
+	public static final int TOO_MANY_REQUESTS = 429;
+	
+	public static final int UNAVAILABLE_FOR_LEGAL_REASONS = 451;
+	
+	// server errors
+	
+	public static final int INTERNAL_SERVER_ERROR = 500;
 
+	public static final int NOT_IMPLEMENTED = 501;
+	
+	public static final int BAD_GATEWAY = 502;
+	
+	public static final int SERVICE_UNAVAILABLE = 503;
+	
+	public static final int GATEWAY_TIMEOUT = 504;
+	
+	public static final int HTTP_VERSION_NOT_SUPPORTED = 505;
+	
 }
