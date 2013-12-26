2cf07318a5e53db71534f51f11155b51a0cce1ae
Sandeep Gupta <sandy.pec@gmail.com>
2013-12-27 00:59:52 +0530

    Updated documentation and error handling

--- src/main/java/com/sangupta/jerry/util/HashUtils.java
+++ src/main/java/com/sangupta/jerry/util/HashUtils.java
@@ -31,10 +31,27 @@ import com.sangupta.jerry.encoder.Base64Encoder;
 
 /**
  * @author sangupta
- *
+ * 
  */
 public class HashUtils {
-	
+
+	/**
+	 * Generate the HMAC_SHA1 signature for the given string using the given key
+	 * string. The method never returns a <code>null</code>.
+	 * 
+	 * @param signable
+	 *            the string to be signed or generated hash of
+	 * 
+	 * @param keyString
+	 *            the key string or secret to use to generate the hash.
+	 * 
+	 * @return the Base64 encoded string representing the hash
+	 * 
+	 * @throws RuntimeException
+	 *             if the algorithm implementation is not found, or the key
+	 *             provided is invalid.
+	 * 
+	 */
 	public static String getHMAC(String signable, String keyString) {
 		SecretKeySpec key = new SecretKeySpec((keyString).getBytes(StringUtils.CHARSET_UTF8), "HmacSHA1");
 		Mac mac;
@@ -44,12 +61,10 @@ public class HashUtils {
 			byte[] bytes = mac.doFinal(signable.getBytes(StringUtils.CHARSET_UTF8));
 			return Base64Encoder.encodeToString(bytes, false);
 		} catch (NoSuchAlgorithmException e) {
-			e.printStackTrace();
+			throw new RuntimeException("This JRE version does not provide an implementation of HMAC-SHA1 algorithm");
 		} catch (InvalidKeyException e) {
-			e.printStackTrace();
+			throw new RuntimeException("Invalid key exception, unable to sign string!");
 		}
-		
-		return null;
 	}
-	
+
 }
